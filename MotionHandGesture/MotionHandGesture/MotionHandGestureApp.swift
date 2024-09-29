f032f5daaaf0bcac3322a5644e820e0086541857
manishparihar948 <manish.parihar948@gmail.com>
2024-09-29 15:39:39 +0200

    Initial project setup

---
+++ MotionHandGesture/MotionHandGesture/MotionHandGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionHandGestureApp.swift
+//  MotionHandGesture
+//
+//  Created by Manish Parihar on 29.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MotionHandGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
