387664d10b3a899df70ca32d47708cda91d97d30
manishparihar948 <manish.parihar948@gmail.com>
2024-09-29 15:53:46 +0200

    Added Camera, MLCamera and Hand Gesture dataset with Machine Learning Model

---
+++ MotionHandGesture/MotionHandGesture/Feature/Camera/MLCamera.swift
@@ -0,0 +1,19 @@
+//
+//  MLCamera.swift
+//  MotionHandGesture
+//
+//  Created by Manish Parihar on 29.09.24.
+//
+
+import Foundation
+import Vision
+
+final class MLCamera: Camera {
+    lazy var handPoseRequest: VNDetectHumanHandPoseRequest = {
+        let request = VNDetectHumanHandPoseRequest()
+        request.maximumHandCount = 1
+        return request
+    }()
+    
+    // var currentMLModel: HandPoseMLModel?
+}
--- MotionHandGesture/MotionHandGesture/MotionHandGestureApp.swift
+++ MotionHandGesture/MotionHandGesture/MotionHandGestureApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct MotionHandGestureApp: App {
+    @State var appModel = AppModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appModel)
+                .task {
+                    await appModel.useLastTrainedModel()
+                }
         }
     }
 }
